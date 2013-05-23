03ae266663da5a35fae4bba6aa5800a792540746
gotclout <rlfosterjr@gmail.com>
2013-05-23 17:28:15 -0300

    Create prime-test.cpp
    compile with g++ -g -lrt prime-test.cpp -o t
    
    Warnings:
    Primes.h: In constructor ‘Primes::Primes()’:
    Primes.h:49: warning: large integer implicitly truncated to unsigned type

---
+++ test/prime-test.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+#include "Timer.h"
+#include "Primes.h"
+
+#define N 100000ULL  //Num Primes
+#define C 1299710ULL //Start Prime Initalization Value
+
+static long long elapsed = 0;
+
+/**
+ * Compute and print first N primes
+ */
+int main()
+{
+  //Try keeping initialization times out of the processing times
+  tulong i  = 1,
+         pr = 1;
+
+  Timer ct, //Computation timer
+        pt; //Rendering timer
+
+  ct.start();//Start Computation
+  Primes p(C);
+  ct.stop();
+
+  cout << "Time to Compute "    << N << " primes\n" << ct
+       << "SIZE : " << p.size() << '\n'
+       << "COUNT: " << p.nump() << '\n'
+       << "MAX:   " << p.maxp() << '\n'; //Misc Stats
+
+  //add this time in and see how close total is to system
+  elapsed += ct.getElapsedNSecs();
+
+  ct.resume();//Rendering
+  for(pr = p.nextprime(pr); i <= N; pr = p.nextprime(pr), i++)
+    i%9 == 0 ? printf("%llu\n", pr) : printf("%llu ", pr);
+  ct.stop();
+
+  cout << "\nTime to Render\n" << ct << "TOTAL: "
+       << "elapsed + ct.getDurationNSecs()\n"
+       << elapsed << " + " << ct.getDurationNSecs() << '\n'
+       << elapsed + ct.getDurationNSecs() << "ns";
+
+  return 0;
+}
